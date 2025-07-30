d72bc4bc7ab7ce3bc991362bf6c84bb3c7186f87
MeetASL11 <meet.makwana@dev.appstonelab.com>
2025-07-30 16:42:35 +0530

    small changes

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/docs",
+        destination: "/docs",
+      },
+      {
+        source: "/docs/:path*",
+        destination: "/docs/:path*",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
